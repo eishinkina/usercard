ab44b20885fb0cd4868462339961546968b43ee5
Ekaterina Ishinkina <esiforlearn@gmail.com>
2024-02-07 10:59:43 +0400

    Refactor UserCard component to display full name in uppercase

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="user-card">
-    <h1>Научу зарабатывать от 50 000р. в час</h1>
-    <div class="main-information">
-      <img src="../assets/images/коуч.jpg" width="500px" />
-      <div class="info">
-        <h2>{{ firstName + " " + secondName + " " + lastName }}</h2>
-        <strong>Бизнес-коуч</strong>
-        <ul class="list">
-          <li>Учу делать РЕАЛЬНЫЕ бабки, на себя просто не хватило времени</li>
-          <li>НЕ знаю, что такое "Успешный Успех", но знаю, что нужно ТЕБЕ!</li>
-          <li>Пообещал, что побреюсь, когда кого-нибудь ОБМАНУ.</li>
-        </ul>
+  <div class="conatiner">
+    <div class="user-card">
+      <h1>Научу зарабатывать от 50 000р. в час</h1>
+      <div class="main-information flex">
+        <img src="../assets/images/коуч.jpg" width="500px" />
+        <div class="info">
+          <h2>
+            {{ fullName }}
+          </h2>
+          <strong>Бизнес-коуч</strong>
+          <ul class="list">
+            <li>
+              Учу делать РЕАЛЬНЫЕ бабки, на себя просто не хватило времени
+            </li>
+            <li>
+              НЕ знаю, что такое "Успешный Успех", но знаю, что нужно ТЕБЕ!
+            </li>
+            <li>Пообещал, что побреюсь, когда кого-нибудь ОБМАНУ.</li>
+          </ul>
+        </div>
       </div>
+      <p>Участников: 0</p>
+      <ul></ul>
+      <!-- input type text -->
     </div>
-    <p>Участников: 0</p>
-    <ul></ul>
-    <!-- input type text -->
   </div>
 </template>
 
@@ -27,6 +35,11 @@ export default {
       secondName: "Иванович",
       lastName: "Иванов",
     };
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName.toUpperCase()} ${this.secondName.toUpperCase()} ${this.lastName.toUpperCase()}`;
+    },
   },
 };
 </script>
